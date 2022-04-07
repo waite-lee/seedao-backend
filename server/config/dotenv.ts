701604e427b51a9fbc5d8e323e4f9c75602ce85f
fatfingererr <fatfingererr@gmail.com>
2022-04-07 16:18:31 +0800

    dotenv using the mode dev and prod

--- server/config/dotenv.ts
+++ server/config/dotenv.ts
@@ -1,4 +1,3 @@
 import dotenv from 'dotenv';
 
-const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env';
-dotenv.config({ path: envFile });
+dotenv.config({ path: process.env.NODE_ENV });
