4da83721f38537fcf4169a2249042544b606c57a
fatfingererr <fatfingererr@gmail.com>
2022-04-07 17:33:40 +0800

    filter with undefined and null

--- server/v1/notion/notion.service.ts
+++ server/v1/notion/notion.service.ts
@@ -1,11 +1,11 @@
 import { Client } from '@notionhq/client';
 import { QueryDatabaseFilterParameters, QueryDatabaseResponse } from '@server/v1/types/notion.types';
 
-const notion = new Client({ auth: process.env.NOTION_API_KEY });
-
 async function fetchNotionData(id: string, filter: QueryDatabaseFilterParameters): Promise<QueryDatabaseResponse> {
     return new Promise<QueryDatabaseResponse>((resolve) => {
-        if (Object.keys(filter).length === 0) {
+        const notion = new Client({ auth: process.env.NOTION_API_KEY });
+        if (filter === undefined || filter === null ||
+            Object.keys(filter).length === 0) {
             resolve(notion.databases.query({ database_id: id }));
         }
         else {
